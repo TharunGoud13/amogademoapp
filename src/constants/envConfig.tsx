d863d13a088b8eca1b85d3d24e32371972eafc4a
TharunGoud13 <amaravaitharun13@gmail.com>
2024-07-29 08:53:10 +0530

    added https

--- src/constants/envConfig.tsx
+++ src/constants/envConfig.tsx
@@ -1,8 +1,8 @@
 
 export const CREATE_USER_API = "https://us4okg8.219.93.129.146.sslip.io/contacts"
 export const GET_ORDER_ITEMS = "https://storesdemo.morr.biz/wp-json/woomorrintegration/v1/orderproducts?order_per_page=10"
-export const GET_CONTACTS_API = "http://no0wgko.219.93.129.146.sslip.io/user_catalog"
-export const GET_ONE_CONTACT_API = "http://no0wgko.219.93.129.146.sslip.io/user_catalog?user_catalog_id=eq."
-export const GET_GROUPS="http://no0wgko.219.93.129.146.sslip.io/chat_group"
-export const GET_SINGLE_GROUP="http://no0wgko.219.93.129.146.sslip.io/chat_group?chat_group_id=eq."
-export const GET_CHAT_GROUP_USERS="http://no0wgko.219.93.129.146.sslip.io/chat_group_users"
+export const GET_CONTACTS_API = "https://no0wgko.219.93.129.146.sslip.io/user_catalog"
+export const GET_ONE_CONTACT_API = "https://no0wgko.219.93.129.146.sslip.io/user_catalog?user_catalog_id=eq."
+export const GET_GROUPS="https://no0wgko.219.93.129.146.sslip.io/chat_group"
+export const GET_SINGLE_GROUP="https://no0wgko.219.93.129.146.sslip.io/chat_group?chat_group_id=eq."
+export const GET_CHAT_GROUP_USERS="https://no0wgko.219.93.129.146.sslip.io/chat_group_users"
