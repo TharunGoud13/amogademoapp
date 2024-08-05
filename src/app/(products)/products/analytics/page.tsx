f2d64957517b1c4e774310d8aa3cae68b0cbc227
TharunGoud13 <amaravaitharun13@gmail.com>
2024-08-05 12:49:59 +0530

    removed redux api response

--- src/app/(products)/products/analytics/page.tsx
+++ src/app/(products)/products/analytics/page.tsx
@@ -1,37 +1,12 @@
 "use client"
-import { fetchDataRequest } from '@/lib/store/actions'
-import React, { FC, useEffect } from 'react'
-import { connect } from 'react-redux'
 
-interface Props{
-  fetchDataRequest:any
-  data:any
-}
-
-const Analytics:FC<Props> = ({fetchDataRequest,data}) => {
-
-  useEffect(() => {
-    fetchDataRequest()
-  },[fetchDataRequest])
-
-  console.log("data----",data)
-  return (
+const Analytics = () => {
+  return(
     <div>
-        
-        <h1>Analytics Page</h1>
-        {data.map((item:any,index:any) => <div key={index}>
-          <p>{item.name}</p>
-        </div>)}
-        
-    
+      Analytics component
     </div>
   )
 }
 
-const mapStateToProps = (state:any) => ({
-  data: state.data
-})
 
-const mapDispatchToProps = {fetchDataRequest}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Analytics)
+export default Analytics
